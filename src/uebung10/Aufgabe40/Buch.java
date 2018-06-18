f2cdcf8c00254f20bd9426dd633da2c7f6e0496a
Samuel <samuel.j.luft@gmail.com>
2018-06-18 18:04:38 +0200

    Aufgabe 40

---
+++ src/uebung10/Aufgabe40/Buch.java
@@ -0,0 +1,15 @@
+package uebung10.Aufgabe40;
+
+public class Buch {
+
+  private String isbn;
+  private String titel;
+
+  public String getTitel() {
+	return titel;
+  }
+
+  public String getIsbn() {
+	return isbn;
+  }
+}
---
+++ src/uebung10/Aufgabe40/LoadSaveException.java
@@ -0,0 +1,8 @@
+package uebung10.Aufgabe40;
+
+public class LoadSaveException extends Exception {
+
+  public LoadSaveException(String message) {
+	super(message);
+  }
+}
